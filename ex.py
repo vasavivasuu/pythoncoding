2b8713e8145da14980d41d78456b3adea97c6f0f
vasavivasuu <vasavi2233@gmail.com>
2019-02-06 16:51:38 +0530

    new files to prita branch

---
+++ ex.py
@@ -0,0 +1,10 @@
+
+country=str(raw_input("enter the country"))
+state=str(raw_input("enter the state"))
+
+if (country=='india' and state=='andhra') :
+   print("14 districts")
+if country=='pak' and state=='pak' :
+   print("12 districts") 
+
+
